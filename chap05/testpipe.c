d6365978d84727ff1a239ef934a4b388aed9ec52
Ye-zixiao <1461018824@qq.com>
2020-11-23 22:43:43 +0800

    date-2020-11-23

--- chap05/testpipe.c
+++ chap05/testpipe.c
@@ -1,5 +1,10 @@
 #include "MyUNP.h"
 
+void sig_pipe(int signo) {	
+	printf("caught SIGPIPE\n");
+	exit(EXIT_FAILURE);
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -8,18 +13,24 @@ int main(int argc, char* argv[])
 
 	if (argc < 2)
 		err_quit("usage: %s <ip-address>", argv[0]);
+	if (mysignal(SIGPIPE, sig_pipe) == SIG_ERR)
+		err_sys("mysignal error");
 
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		err_sys("socket error");
 	bzero(&svaddr, sizeof(svaddr));
 	svaddr.sin_family = AF_INET;
-	svaddr.sin_port = htons(12000);
+	svaddr.sin_port = htons(9999);
 	if (inet_pton(AF_INET, argv[1], &svaddr.sin_addr) <= 0)
 		err_sys("inet_pton convert string %s to address error", argv[1]);
 	if (connect(sockfd, (struct sockaddr*)&svaddr, sizeof(svaddr)) == -1)
 		err_sys("connect error");
 	
-	str_cli(sockfd, stdin);
+	sleep(2);
+	write(sockfd, "f", 1);
+	sleep(2);
+	write(sockfd, "f", 1);
+
 	if (close(sockfd) == -1)
 		err_sys("close error");
 	exit(EXIT_SUCCESS);
--- include/MyUNP.h
+++ include/MyUNP.h
@@ -54,16 +54,6 @@ void debug(void);
 
 /* 网络地址辅助函数  */
 char* sock_ntop(const struct sockaddr* sockaddr, socklen_t addrlen);
-//下面的暂不实现
-int sock_bind_wild(int sockfd, int family);
-int sock_cmp_addr(const struct sockaddr* sockaddr1, 
-				const struct sockaddr* sockaddr2, socklen_t addrlen);
-int sock_cmp_port(const struct sockaddr* sockaddr1,
-				const struct sockaddr* sockaddr2, socklen_t addrlen);
-int sock_get_port(const struct sockaddr* sockaddr, socklen_t addrlen);
-void sock_set_addr(struct sockaddr* sockaddr, socklen_t addrlen, void* ptr);
-void sock_set_port(struct sockaddr* sockaddr, socklen_t addrlen, in_port_t port);
-void sock_set_wild(struct sockaddr* sockaddr, socklen_t addrlen);
 
 
 /* 流式套接字指定字节的读写 */
@@ -87,12 +77,12 @@ Sigfunc* mysignal(int signo, Sigfunc* func);
 /* 回射客户-服务器辅助函数 */
 void str_echo(int sockfd);
 
-void sum_echo1(int sockfd);
-void sum_echo2(int sockfd);
-
 void str_cli(int sockfd, FILE* fp);
 void str_cli1(int sockfd, FILE* fp);
 void str_cli2(int sockfd, FILE* fp);
+
+void sum_echo1(int sockfd);
+void sum_echo2(int sockfd);
 
 void sum_cli2(int sockfd, FILE* fp);
 
